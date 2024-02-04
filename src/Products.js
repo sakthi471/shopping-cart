9bbcee3a7ab9d4143c3769f55d366ff08b690858
sakthi471 <sakthim61797@gmail.com>
2024-02-04 23:59:36 +0530

    first commit

---
+++ src/Products.js
@@ -0,0 +1,110 @@
+/*============ Build our products data ==============*/
+
+export const productsData = [
+  {
+    id: 1,
+    title: "Tv",
+    price: 40000,
+    imageUrl:
+      "./images/tv.webp",
+  },
+
+  {
+    id: 2,
+    title: "Watch",
+    price: 5000,
+    imageUrl: "./images/watch.jpg",
+  },
+
+  {
+    id: 3,
+    title: "Redmi mobile",
+    price: 13000,
+    imageUrl: "./images/phone.jpg",
+  },
+
+  {
+    id: 4,
+    title: "Router",
+    price: 20000,
+    imageUrl: "./images/router.webp",
+  },
+
+  {
+    id: 5,
+    title: "HP Mouse",
+    price: 3500,
+    imageUrl:
+      "./images/mouse.jpg",
+  },
+
+  {
+    id: 6,
+    title: "Realme earpods",
+    price: 4000,
+    imageUrl: "./images/airpods.jpg",
+  },
+
+  {
+    id: 7,
+    title: "Harry potter collections",
+    price: 2000,
+    imageUrl: "./images/book.jpg",
+  },
+
+  {
+    id: 8,
+    title: "Luxury sofa",
+    price: 10000,
+    imageUrl: "./images/sofa.jpg",
+  },
+
+  {
+    id: 9,
+    title: "Asus keyboard",
+    price: 2000,
+    imageUrl: "./images/keyboard.webp",
+  },
+
+  {
+    id: 10,
+    title: "Silentnight Ottoman Base",
+    price: 50000,
+    imageUrl: "./images/650$_silentnight-ottoman-base.jpg",
+  },
+
+  {
+    id: 11,
+    title: "CEAT Bat",
+    price: 4000,
+    imageUrl: "./images/bat.jpg",
+  },
+
+  {
+    id: 12,
+    title: "NIVIA Football",
+    price: 2000,
+    imageUrl: "./images/football.webp",
+  },
+
+  {
+    id: 13,
+    title: "Acer laptop",
+    price: 45000,
+    imageUrl: "./images/laptop.jpg",
+  },
+
+  {
+    id: 14,
+    title: "Samsung Tab",
+    price: 25000,
+    imageUrl: "./images/tab.jpg",
+  },
+
+  {
+    id: 15,
+    title: "Classic Ottoman Base",
+    price: 40000,
+    imageUrl: "./images/350$_classic-ottoman-base.jpg",
+  },
+];
